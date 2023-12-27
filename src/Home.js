3858b28c10d836cc372980c1f917eb47e61ff2e4
srinikethanreddy01 <153156304+srinikethanreddy01@users.noreply.github.com>
2023-12-27 10:45:05 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,85 +1,3 @@
-// import React, { useState } from 'react'
-// import { toast } from 'react-hot-toast'
-// import axios from 'axios'
-// import './home.css'
-
-// function Home() {
-//     const [image, setImage] = useState(null);
-//     const [prediction, setPrediction] = useState(null);
-//     const [target, setTarget] = useState(null);
-
-//     const handleImageChange = (e) => {
-//         const file = e.target.files[0];
-
-//         setImage(file);
-//     };
-
-//     const handlesubmit = async (e) => {
-//         e.preventDefault();
-//         try {
-//             const reader = new FileReader();
-//             reader.onloadend = () => {
-//                 const base64data = reader.result.split(',')[1];
-
-//                 const requestData = {
-//                     image: base64data,
-//                     name: image.name || 'default_filename.jpg',
-//                 };
-
-
-//                 axios.post(process.env.FLASK_URL,requestData, {
-//                     withCredentials: true,
-//                 })
-//                     .then((response) => {
-//                         const result = response;
-                        
-//                         console.log(Object.keys(result))
-                        
-//                         setPrediction(result["data"]["prediction"])
-//                         setTarget(result["data"]["target"])
-//                         console.log('Response from server:', result);
-//                     })
-//                     .catch((error) => {
-//                         console.error('Error:', error);
-//                     });
-//             };
-
-//             reader.readAsDataURL(image);
-//         } catch (error) {
-//             console.log(error);
-//         }
-
-
-
-        
-        
-
-
-
-
-//     }
-//     return (
-//         <div>
-//             <form onSubmit={handlesubmit}>
-//                 <label htmlFor='image'>Upload image</label>
-//                 <input type='file' id="image" onChange={handleImageChange}></input>
-//                 <button type="submit">Submit</button>
-//             </form>
-
-//             {prediction ? (
-//                 <div className='output'>
-                    
-//                     <h2>prediction</h2>
-//                     {prediction}
-//                     {/* <h2>target</h2>
-//                     {target} */}
-//                 </div>
-//             ) : null}
-//         </div>
-//     )
-// }
-
-// export default Home
 
 
 import React, { useState } from 'react'
@@ -112,7 +30,7 @@ function Home() {
         formData.append("image", imgFile);
 
         try {
-            const response = await fetch("https://barely-ruling-whale.ngrok-free.app/upload", {
+            const response = await fetch("barely-ruling-whale.ngrok-free.app/upload", {
             method: "POST",
             body: formData,
             });
